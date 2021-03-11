69477efd6bd1429ef0d6e947eb7e14733acf4b62
Rodion <rprosvirnin@zeyden.online>
2021-03-11 16:29:57 +0300

    Initial commit

---
+++ app/src/main/java/ru/rodni_it/composelearning/MainActivity.kt
@@ -0,0 +1,38 @@
+package ru.rodni_it.composelearning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsStory()
+        }
+    }
+}
+
+@Composable
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    NewsStory()
+}
