d83d7ac19dfe794f817e0efa4bc27da5f23b64cc
Rodion <rprosvirnin@zeyden.online>
2021-03-11 17:16:37 +0300

    learned compose basics.

--- app/src/main/java/ru/rodni_it/composelearning/MainActivity.kt
+++ app/src/main/java/ru/rodni_it/composelearning/MainActivity.kt
@@ -1,13 +1,20 @@
 package ru.rodni_it.composelearning
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,12 +29,35 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Text("A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+                style = typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis)
+            Text("Davenport, California",
+                style = typography.body2)
+            Text("December 2018",
+                style = typography.body2)
+        }
     }
 }
 
